c2230379b901e29ff7799e4d7ae8b9a8279860b8
Lawrence <Lawrence.martin.green@gmail.com>
2022-04-16 13:47:07 +0100

    SkeletonRepo

---
+++ Include/MesoXML.h
@@ -0,0 +1,9 @@
+/*************************************************************************************************************
+ * MesoXML
+ * The absolutly worst XML parser you have ever seen in your life! I would recommend not using this tbh
+ *
+ * License   : MIT
+ * Copyright : 2022 Mesopotamic   
+ * Authours  :
+ *		Lawrence G
+ *************************************************************************************************************/
---
+++ README.md
@@ -0,0 +1,3 @@
+# MesoXML
+
+The Mesopotamic XML parser. This is only really used to parse the vk.xml registry. Guess what, this is gonna be bad! But hey it's MIT licensed :)
